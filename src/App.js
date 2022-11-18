5c72b2da143ec014946734046b6a7e5b6b6d6b92
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-18 19:15:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../src/assets/css/main.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import MainContent from './pages/MainContent'
+
+function App () {
+  return (
+    <div className="container App shadow">
+      <MainContent />
+    </div>
+  )
+}
+
+export default App
