a5478defbd8f400587f978d26077d311277ba246
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-23 19:44:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import '../src/assets/css/main.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainContent from './pages/MainContent'
+import Checkout from './pages/Checkout'
 
 function App () {
   return (
-    <div className="container App shadow">
-      <MainContent />
+    <div className="App shadow">
+      <Router>
+        <Routes>
+          <Route path='/' element={<MainContent />} />
+          <Route path='/checkout' element={<Checkout />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
