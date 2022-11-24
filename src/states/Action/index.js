ec37228bbb5b97cff1eac72e62a37b2fe56b0b6e
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-24 15:17:57 +0530

    Add files via upload

--- src/states/Action/index.js
+++ src/states/Action/index.js
@@ -1,22 +1,22 @@
 import { ActionTypes } from './types'
 
-export const getCategory = async (data) => {
+export const getCategory = (data) => {
   return {
     type: ActionTypes.GET_CATEGORY,
     payload: data
   }
 }
 
-export const getProduct = async (data) => {
+export const getProduct = (data) => {
   return {
     type: ActionTypes.GET_PRODUCT,
     payload: data
   }
 }
 
-export const setCart = (data, pop, checkbox) => {
+export const setCart = (data, pop, checkbox, name, id) => {
   return {
     type: ActionTypes.SET_CART,
-    payload: { data, pop, checkbox }
+    payload: { data, pop, checkbox, name, id }
   }
 }
--- src/states/Reducers/cart.js
+++ src/states/Reducers/cart.js
@@ -5,10 +5,25 @@ const initialCart = {
 }
 
 export const cartReducer = (state = initialCart, { type, payload }) => {
-  // console.log('action >> ', type)
+  // console.log('action >> ', payload.id)
   switch (type) {
     case ActionTypes.SET_CART:
-      return { ...state, cart: payload }
+      var IteamIndex = state.cart.findIndex((iteam) => iteam.pop.id === payload.id || iteam.name.name === payload.name)
+      console.log('IteamIndex >> ', IteamIndex)
+      if (IteamIndex >= 0) {
+        state.cart[IteamIndex].qnty += 1
+        return {
+          ...state,
+          cart: [...state.cart, payload]
+        }
+      } else {
+        const temp = { ...payload, qnty: 1 }
+        // state.cart.push(temp)
+        return {
+          ...state,
+          cart: [...state.cart, temp]
+        }
+      }
     default:
       return state
   }
