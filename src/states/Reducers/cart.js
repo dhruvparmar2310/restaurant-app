f72a3eee8adf32594d3626e9700acefdf0ed17db
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-25 19:13:19 +0530

    Add files via upload

--- src/states/Reducers/cart.js
+++ src/states/Reducers/cart.js
@@ -9,26 +9,26 @@ export const cartReducer = (state = initialCart, { type, payload }) => {
   // console.log('action >> ', payload.id)
   switch (type) {
     case ActionTypes.SET_CART:
-      var IteamIndex = state.cart.findIndex((iteam) => iteam.id === payload.id && iteam.sizeData.name === payload.sizeData.name && iteam.checkbox.name === payload.checkbox.name)
-      console.log('IteamIndex >> ', IteamIndex)
-      if (IteamIndex >= 0) {
-        state.cart[IteamIndex].qnty += 1
-        return {
-          ...state,
-          cart: [...state.cart, payload]
-        }
-      } else {
-        const temp = { ...payload, qnty: 1 }
-        // state.cart.push(temp)
-        return {
-          ...state,
-          cart: [...state.cart, temp]
-        }
-      }
-      // return {
-      //   ...state,
-      //   cart: [...state.cart, payload]
+      // var IteamIndex = state.cart.findIndex((iteam) => iteam.id === payload.id && iteam.sizeData.name === payload.sizeData.name && iteam.checkbox.name === payload.checkbox.name)
+      // console.log('IteamIndex >> ', IteamIndex)
+      // if (IteamIndex >= 0) {
+      //   state.cart[IteamIndex].quantity += 1
+      //   return {
+      //     ...state,
+      //     cart: [...state.cart, payload]
+      //   }
+      // } else {
+      //   const temp = { ...payload, quantity: 1 }
+      //   // state.cart.push(temp)
+      //   return {
+      //     ...state,
+      //     cart: [...state.cart, temp]
+      //   }
       // }
+      return {
+        ...state,
+        cart: [...state.cart, payload]
+      }
     default:
       return state
   }
