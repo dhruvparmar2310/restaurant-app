791042d53a44e92efefbc1039ee5fa3456054d9c
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-28 19:06:01 +0530

    Add files via upload

--- src/states/Reducers/cart.js
+++ src/states/Reducers/cart.js
@@ -9,26 +9,28 @@ export const cartReducer = (state = initialCart, { type, payload }) => {
   // console.log('action >> ', payload.id)
   switch (type) {
     case ActionTypes.SET_CART:
-      // var IteamIndex = state.cart.findIndex((iteam) => iteam.id === payload.id && iteam.sizeData.name === payload.sizeData.name && iteam.checkbox.name === payload.checkbox.name)
-      // console.log('IteamIndex >> ', IteamIndex)
-      // if (IteamIndex >= 0) {
-      //   state.cart[IteamIndex].quantity += 1
-      //   return {
-      //     ...state,
-      //     cart: [...state.cart, payload]
-      //   }
-      // } else {
-      //   const temp = { ...payload, quantity: 1 }
-      //   // state.cart.push(temp)
-      //   return {
-      //     ...state,
-      //     cart: [...state.cart, temp]
-      //   }
-      // }
-      return {
-        ...state,
-        cart: [...state.cart, payload]
+      // here iteam is nothing, it will display when second time something is added.
+      var IteamIndex = state.cart.findIndex((iteam) => iteam.id === payload.id && iteam.sizeData.name === payload.sizeData.name)
+      console.log('IteamIndex >> ', IteamIndex)
+      if (IteamIndex >= 0) {
+        state.cart[IteamIndex].qnty += 1
+        state.cart[IteamIndex].sum = state.cart[IteamIndex].sum * 2 
+        return {
+          ...state,
+          cart: [...state.cart, payload]
+        }
+      } else {
+        const temp = { ...payload, qnty: 1 }
+        // state.cart.push(temp)
+        return {
+          ...state,
+          cart: [...state.cart, temp]
+        }
       }
+      // return {
+      //   ...state,
+      //   cart: [...state.cart, payload]
+      // }
     default:
       return state
   }
