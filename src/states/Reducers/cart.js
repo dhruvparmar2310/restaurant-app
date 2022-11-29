c7fe557fa957a72f5351971c80839cbaac2fc469
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-29 19:22:47 +0530

    Add files via upload

--- src/states/Reducers/cart.js
+++ src/states/Reducers/cart.js
@@ -10,27 +10,24 @@ export const cartReducer = (state = initialCart, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_CART:
       // here iteam is nothing, it will display when second time something is added.
-      var IteamIndex = state.cart.findIndex((iteam) => iteam.id === payload.id && iteam.sizeData.name === payload.sizeData.name)
-      console.log('IteamIndex >> ', IteamIndex)
-      if (IteamIndex >= 0) {
-        state.cart[IteamIndex].qnty += 1
-        state.cart[IteamIndex].sum = state.cart[IteamIndex].sum * 2 
+      var dataIndex = state.cart.findIndex((data) => data.id === payload.id && data.sizeData.name === payload.sizeData.name && data.productId === payload.productId)
+      // console.log('dataIndex >> ', dataIndex)
+      if (dataIndex >= 0) {
+        state.cart[dataIndex].qnty += 1
+        state.cart[dataIndex].itemCount += 1
+        // don't return again payload after updating...
         return {
           ...state,
-          cart: [...state.cart, payload]
+          cart: [...state.cart]
         }
       } else {
         const temp = { ...payload, qnty: 1 }
         // state.cart.push(temp)
         return {
-          ...state,
+          ...state, 
           cart: [...state.cart, temp]
         }
       }
-      // return {
-      //   ...state,
-      //   cart: [...state.cart, payload]
-      // }
     default:
       return state
   }
