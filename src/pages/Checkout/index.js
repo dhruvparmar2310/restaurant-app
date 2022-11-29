8d40cc43d73add9c01a7fda77e4d215c1a86c21a
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-29 19:21:11 +0530

    Add files via upload

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -14,9 +14,10 @@ export default function Checkout () {
   // const [tempArray, setTempArray] = useState([])
   const [totalPrice, setTotalPrice] = useState(0)
   const [totalItem, setTotalItem] = useState(0)
+  const [tableNumber, setTableNumber] = useState(null)
 
   const checkoutData = useSelector((state) => state.allCart.cart)
-  console.log('checkoutData :>> ', checkoutData)
+  // console.log('checkoutData :>> ', checkoutData)
   const navigate = useNavigate()
 
   const ConfirmOrder = (e) => {
@@ -38,11 +39,19 @@ export default function Checkout () {
     const countItem = checkoutData.length !== 0 ? checkoutData.map((data) => data.itemCount) : ''
     const totalItem = countItem.length !== 0 ? countItem.reduce((a, b) => a + b) : ''
     setTotalItem(totalItem)
+
+    // random number
+    if (checkoutData.length !== 0) {
+      const tableNumber = Math.floor(Math.random() * 50) + 10
+      setTableNumber(tableNumber)
+    } else {
+      setTableNumber('!')
+    }
   }, [totalPrice, totalItem, checkoutData])
 
   const handlePlaceOrder = (e) => {
     if (checkoutData.length !== 0) {
-      alert('Order added Successfully')
+      alert('Bill\n------------------------------------\n\nQuatity : ' +totalItem+'\nGrand Total : ' + totalPrice)
     } else {
       alert('You have not selected any item.')
     }
@@ -52,11 +61,11 @@ export default function Checkout () {
     <div className='checkout-main'>
       <div className='main-content'>
         <div className='row mt-2'>
-          <div className='col-lg-3 goBack' onClick={(e) => handleGoBack(e)}><MdArrowBackIosNew /></div>
-          <div className='col-lg-6'>
+          <div className='col-lg-3 col-md-3 col-sm-3 col-xs-3 goBack' onClick={(e) => handleGoBack(e)}><MdArrowBackIosNew /></div>
+          <div className='col-lg-6 col-md-6 col-sm-6 col-xs-6 header'>
             <h1>Checkout</h1>
           </div>
-          <div className='col-lg-3'><HiDotsHorizontal /></div>
+          <div className='col-lg-3 col-md-3 col-sm-3 col-xs-3 option'><HiDotsHorizontal /></div>
         </div>
         <div className='hotel row'>
           <div className='col-lg-12 hotel-name'>
@@ -68,30 +77,40 @@ export default function Checkout () {
         </div>
         <div className='row product-details'>
             {checkoutData.length !== 0 ? checkoutData?.map((value, index) => {
-              console.log('value >> ', value)
+              // console.log('value >> ', value)
               return (
                 <React.Fragment key={index}>
                   <h5 className='mt-2'>
-                  {/* {value.name.map((data, index) => {
-                    return (
-                      <React.Fragment key={index}>
-                        {data.name} ({data.id})
-                      </React.Fragment>
-                    )
-                  })} */}
-                  {value.name} ({value.itemCount})
+                    {value.name} ({value.itemCount})
                   </h5>
-                  <div className='col-lg-6 details'>
+                  <div className='col-lg-6 col-md-6 col-sm-6 col-xs-6 details'>
                     <h6>{value.qnty} x {value.popUpDetails.name}</h6>
                     <p>{value.sizeData.name}, {value.optionData.map((data, index) => <span key={index}>{data.name.concat(' ')}</span>)}</p>
                   </div>
-                  <div className='col-lg-6 price'>
+                  <div className='col-lg-6 col-md-6 col-sm-6 col-xs-6 price'>
                     <p><TbCurrencyPound /> {value.sum}</p>
                   </div>
                 </React.Fragment>
               )
-            }) : <h5 className='text-center'>Your cart is empty</h5>}
+            }) : <h5 className='text-center empty'>Your cart is empty</h5>}
         </div>
+        {toggle
+          ? <>
+              <div className='pop-left'>
+                <div className='pop-left-header'>
+                  <h1>Confirm Order</h1>
+                  <img src={thumb} className='img-fluid' alt='' />
+                </div>
+                <div className='content'>
+                  <p>By placing this order you<br/>agree that you are present in<br/>Kings Arms and over 18 years<br/>old.</p>
+                  <div className='button-controls'>
+                    <button className='btn btnCancel' onClick={() => setToggle(false)}>Cancel</button>
+                    <button className='btn btnPlaceOrder' onClick={(e) => handlePlaceOrder(e)}>Place Order</button>
+                  </div>
+                </div>
+              </div>
+            </>
+          : ''}
         <span className='underline'></span>
         <div className='addNotes'>
           <h5>Add notes:</h5>
@@ -102,28 +121,11 @@ export default function Checkout () {
           <div className='col-lg-9 table-name'>
             <h5>Table Number</h5>
           </div>
-          <div className='col-lg-3'>
-            <button className='btn'>32</button>
+          <div className='col-lg-3 table-number'>
+            <button className='btn'>{tableNumber}</button>
           </div>
         </div>
       </div>
-      {toggle
-            ? <>
-                <div className='pop-left'>
-                  <div className='pop-left-header'>
-                    <h1>Confirm Order</h1>
-                    <img src={thumb} className='img-fluid' alt='' />
-                  </div>
-                  <div className='content'>
-                    <p>By placing this order you<br/>agree that you are present in<br/>Kings Arms and over 18 years<br/>old.</p>
-                    <div className='button-controls'>
-                      <button className='btn btnCancel' onClick={() => setToggle(false)}>Cancel</button>
-                      <button className='btn btnPlaceOrder' onClick={(e) => handlePlaceOrder(e)}>Place Order</button>
-                    </div>
-                  </div>
-                </div>
-              </>
-            : ''}
       <div className='footer' onClick={(e) => ConfirmOrder(e)}>
         <p>Confirm Order</p>
         <p><TbCurrencyPound /> {totalPrice}/ {totalItem} Items</p>
