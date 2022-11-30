f38ad245e709d24ac0ae005ab4b65650a295e4c8
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-30 11:32:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainContent from './pages/MainContent'
 import Checkout from './pages/Checkout'
+import LoadingScreen from './shared/components/LoadingScreen'
 
 function App () {
   return (
@@ -11,6 +12,7 @@ function App () {
       <Router>
         <Routes>
           <Route path='/' element={<MainContent />}/>
+          <Route path='/loading' element={<LoadingScreen />} />
           <Route path='/checkout' element={<Checkout />} />
         </Routes>
       </Router>
