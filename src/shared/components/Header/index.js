cd331880230182bac3aa412d4c220abe08c4e275
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-30 19:13:48 +0530

    Add files via upload

--- src/shared/components/Header/index.js
+++ src/shared/components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import translate from '../../../i18n/translate'
+import { HiDotsHorizontal } from 'react-icons/hi'
 import './style.css'
 
 export default function Header () {
   return (
     <>
       <div className='header'>
-        <h4>Kings Arms Cardington</h4>
-        <p>134 High Street, Kempston, Bedford, <br/>Bedfordshine, MK42 7BN</p>
+        {/* <h4>Kings Arms Cardington</h4> */}
+        <h4>{translate('restaurantName')}</h4>
+        <p>{translate('restaurantAddress', {break: <br />})}</p>
       </div>
     </>
   )
