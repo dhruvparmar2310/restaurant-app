7c3dd6b8d472460ca15e9f93013ffa544f1028b3
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-30 19:09:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainContent from './pages/MainContent'
 import Checkout from './pages/Checkout'
 import LoadingScreen from './shared/components/LoadingScreen'
+import DropDown from './shared/components/DropDown'
 
 function App () {
   return (
@@ -14,6 +15,7 @@ function App () {
           <Route path='/' element={<MainContent />}/>
           <Route path='/loading' element={<LoadingScreen />} />
           <Route path='/checkout' element={<Checkout />} />
+          <Route path='/checkout/option' element={<DropDown />} />
         </Routes>
       </Router>
     </div>
