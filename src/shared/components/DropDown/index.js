a6f3fc4fae7765fc40efca84921f8bdcb4625f19
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-12-01 11:16:29 +0530

    Add files via upload

--- src/shared/components/DropDown/index.js
+++ src/shared/components/DropDown/index.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { LOCALES } from '../../../i18n'
+import { LangContext } from '../../../App'
+import './style.css'
 
 export default function DropDown() {
+   const [selected, setSelected] = useState(LOCALES.ENGLISH)
+   const useLang = useContext(LangContext)
+   
+   const handleLanguage = (e) => {
+    setSelected(e.target.value)
+  }
+  localStorage.setItem('language', selected)
+  useLang.lang(selected)
+
   return (
-    <div>DropDown</div>
+    <div className="dropdown">
+      <button className="btn btn-sm dropbtn">...</button>
+      <select className="dropdown-content" value={selected} onChange={(e)=> handleLanguage(e)}>
+        <option value={LOCALES.ENGLISH}>English</option>
+        <option value={LOCALES.GUJARATI}>Gujarati</option>
+        <option value={LOCALES.HINDI}>Hindi</option>
+      </select>
+    </div>
   )
 }
